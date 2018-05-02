19da63989e74851e4d57497ba95596dc4c3c9e5e
Kai Hendry <hendry@webconverger.com>
2018-05-02 16:16:13 +0800

    creating Duplicates

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
 
@@ -16,6 +17,17 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type listInvitesResponse []struct {
+	ID         string `json:"_id"`
+	InvitedBy  int    `json:"invitedBy"`
+	Invitee    int    `json:"invitee"`
+	Role       string `json:"role"`
+	IsOccupant bool   `json:"isOccupant"`
+	CaseID     int    `json:"caseId"`
+	UnitID     int    `json:"unitId"`
+	Type       string `json:"type"`
+}
+
 func init() {
 	if os.Getenv("UP_STAGE") == "" {
 		log.SetHandler(text.Default)
@@ -27,14 +39,14 @@ func init() {
 func main() {
 	addr := ":" + os.Getenv("PORT")
 	app := pat.New()
-	app.Get("/", listjson)
+	app.Get("/", processPendingInvites)
 	if err := http.ListenAndServe(addr, app); err != nil {
 		log.WithError(err).Fatal("error listening")
 	}
 
 }
 
-func set(me string) error {
+func set(lr listInvitesResponse) error {
 
 	db, err := sql.Open("mysql", os.Getenv("DSN"))
 	if err != nil {
@@ -50,63 +62,75 @@ func set(me string) error {
 		return err
 	}
 
-	result, err := db.Exec(
-		"INSERT INTO ut_test_foo_bar (foobar_invitation_id) VALUES (?)",
-		me,
-	)
-	if err != nil {
-		return err
-	}
+	for _, invite := range lr {
+		log.Infof("Processing invite: %+v", invite)
 
-	log.Infof("%v", result)
-
-	var (
-		id   int
-		name string
-	)
-
-	rows, err := db.Query("select foobar_id, foobar_invitation_id from ut_test_foo_bar")
-	if err != nil {
-		log.WithError(err).Error("failed to open database")
-		return err
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&id, &name)
-
+		result, err := db.Exec(
+			`INSERT INTO ut_invitation_api_data (mefe_invitation_id,
+			bzfe_invitor_user_id,
+			bz_user_id,
+			user_role_type_id,
+			is_occupant,
+			bz_case_id,
+			bz_unit_id,
+			invitation_type,
+			is_mefe_only_user,
+			user_more
+		) VALUES (?,?,?,?,?,?,?,?,?,?)`,
+			invite.ID,
+			invite.InvitedBy,
+			invite.Invitee,
+			invite.Role,
+			invite.IsOccupant,
+			invite.CaseID,
+			invite.UnitID,
+			invite.Type,
+			1,
+			"Use Unee-T for a faster reply",
+		)
 		if err != nil {
-			log.WithError(err).Error("failed to scan")
 			return err
 		}
-		log.Infof("%d %s", id, name)
+
+		log.Infof("Exec result %v", result)
+
 	}
 
-	err = rows.Err()
 	return err
 
 }
 
-func listjson(w http.ResponseWriter, r *http.Request) {
+func getInvites() (lr listInvitesResponse, err error) {
+	resp, err := http.Get(os.Getenv("DOMAIN") + "/api/pending-invitations?accessToken=" + os.Getenv("API_ACCESS_TOKEN"))
+	if err != nil {
+		return lr, err
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&lr)
+	return lr, err
+}
+
+func processPendingInvites(w http.ResponseWriter, r *http.Request) {
 
 	if os.Getenv("UP_STAGE") != "production" {
 		w.Header().Set("X-Robots-Tag", "none")
 	}
 
-	value := r.URL.Query().Get("v")
-	if value == "" {
-		response.BadRequest(w, "Input parameter v is empty.")
-		return
-	}
-
-	err := set(value)
+	// Input
+	lr, err := getInvites()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	log.Infof("Input %+v", lr)
+
+	err = set(lr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	response.OK(w, "Worked")
 
 }
