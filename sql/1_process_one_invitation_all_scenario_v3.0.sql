0dc25e9b7c6e1c724c9aa3e197b503e65b3ca244
franck-boullier <franck.boullier@gmail.com>
2018-06-01 15:36:05 +0800

    use the procedure to create the table that store the dummy user ids

--- sql/1_process_one_invitation_all_scenario_v3.0.sql
+++ sql/1_process_one_invitation_all_scenario_v3.0.sql
@@ -90,26 +90,9 @@
 	SET @timestamp = NOW();
 	
 # We create a temporary table to record the ids of the dummy users in each environments:
-	/*Table structure for table `ut_temp_dummy_users_for_roles` */
-		DROP TABLE IF EXISTS `ut_temp_dummy_users_for_roles`;
+	CALL `create_temp_table_to_update_permissions`; 
 
-		CREATE TABLE `ut_temp_dummy_users_for_roles` (
-		  `environment_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id of the environment',
-		  `environment_name` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-		  `tenant_id` int(11) NOT NULL,
-		  `landlord_id` int(11) NOT NULL,
-		  `contractor_id` int(11) NOT NULL,
-		  `mgt_cny_id` int(11) NOT NULL,
-		  `agent_id` int(11) DEFAULT NULL,
-		  PRIMARY KEY (`environment_id`)
-		) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-	/*Data for the table `ut_temp_dummy_users_for_roles` */
-		INSERT INTO `ut_temp_dummy_users_for_roles`(`environment_id`,`environment_name`,`tenant_id`,`landlord_id`,`contractor_id`,`mgt_cny_id`,`agent_id`) values 
-			(1,'DEV/Staging',96,94,93,95,92),
-			(2,'Prod',93,91,90,92,89),
-			(3,'demo/dev',4,3,5,6,2);
-	
+		
 # The reference of the record we want to update in the table ''
 	SET @reference_for_update = (SELECT `id` FROM `ut_invitation_api_data` WHERE `mefe_invitation_id` = @mefe_invitation_id);	
 
