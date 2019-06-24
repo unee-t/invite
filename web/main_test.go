1176e68c1ef1952f50a2b6850cba312ffbe8e8f2
Kai Hendry <kai.hendry@unee-t.com>
2019-06-24 16:51:29 +0800

    Refactor tests in light of move to prometheus style /metrics

--- web/main_test.go
+++ web/main_test.go
@@ -16,38 +16,44 @@ import (
 	"github.com/unee-t/invite"
 )
 
-func TestHealthCheck(t *testing.T) {
+func TestPOST500(t *testing.T) {
 	ctx := context.Background()
 	h, err := invite.New(ctx)
 	if err != nil {
 		log.WithError(err).Fatal("error setting configuration")
 		return
 	}
-
-	defer h.DB.Close()
-
+	// Database should become unavailable and result in 500s
+	h.DB.Close()
 	app := h.BasicEngine()
-
 	server := httptest.NewServer(app)
 	defer server.Close()
-	resp, err := http.Get(server.URL + "/health_check")
+	// Delete
+	// map[_id:zbHFMYRpSiHmMNzgh caseId:0 invitedBy:638 invitee:542 isOccupant:false mefeInvitationIdIntValue:36624 role:Management Company type:remove_user unitId:105]
+	// Put back
+	// map[_id:6Z7e5ExhE4fP9L6R2 caseId:0 invitedBy:638 invitee:542 isOccupant:false mefeInvitationIdIntValue:36625 role:Management Company type:keep_default unitId:105]
+	invites := []invite.Invite{{
+		ID:         fmt.Sprintf("%s", uuid.Must(uuid.NewV4())), // Error 1062: Duplicate entry 'zbHFMYRpSiHmMNzgh' for key 'mefe_invitation_id_must_be_unique'
+		InvitedBy:  638,
+		Invitee:    542,
+		Role:       "Management Company",
+		IsOccupant: false,
+		CaseID:     0,
+		UnitID:     105,
+		Type:       "remove_user",
+	}}
+
+	b, err := json.Marshal(&invites)
+	if err != nil {
+		t.Fatalf("Marshal to JSON error: %v", err)
+	}
+	resp, err := http.Post(server.URL, "application/json", bytes.NewBuffer(b))
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("got %d, want %d", resp.StatusCode, http.StatusOK)
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Fatalf("got %d, want %d", resp.StatusCode, http.StatusInternalServerError)
 	}
-
-	actual, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expected := "OK"
-	if !strings.Contains(string(actual), expected) {
-		t.Errorf("got %s, want %s", actual, expected)
-	}
-
 }
 
 func TestPOSTdelete(t *testing.T) {
